30601d47fd2690024d6b741701ffe9d545d76224
dmitry-kurmanov <kurmanov.work@gmail.com>
2024-08-07 12:41:25 +0400

    Initial commit

---
+++ src/lib/Survey.svelte
@@ -0,0 +1,34 @@
+<script>
+  import { Model } from "survey-core";
+  import "survey-js-ui";
+  import "survey-core/defaultV2.min.css";
+  import { onMount } from 'svelte';
+
+  export let surveyElement;
+
+  const json = {
+  "elements": [
+      {
+        "type": "radiogroup",
+        "name": "car",
+        "title": "Which is the brand of your car?",
+        "isRequired": true,
+        "showNoneItem": true,
+        "showOtherItem": true,
+        "colCount": 1,
+        "choices": [ "Ford", "Vauxhall", "Volkswagen", "Nissan", "Audi", "Mercedes-Benz", "BMW", "Peugeot", "Toyota" ],
+        "separateSpecialChoices": true,
+        "showClearButton": true
+      }
+    ],
+    "showQuestionNumbers": false
+  };
+  const survey = new Model(json);
+  
+  onMount(() => {
+		survey.render(surveyElement);
+	});
+
+</script>
+
+<div bind:this={surveyElement} class="survey-container"></div> 
